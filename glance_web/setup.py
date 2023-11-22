8524d01f11b8f7e29f11a3369bcd886108d79d3a
MariiaLevytska99 <mariia.levytska99@gmail.com>
2023-11-22 20:30:39 +0200

    test"

--- glance_web/setup.py
+++ glance_web/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glance_web',
-    version='0.10.4',
+    version='0.0.1',
     description='glance_web package',
     author='Mariia Levytska',
     author_email='mariia.leytska@mail.com',
