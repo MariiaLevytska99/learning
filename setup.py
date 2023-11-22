299ac61f9d7a98a2ce852eadc5c27d4d903e3e28
MariiaLevytska99 <mariia.levytska99@gmail.com>
2023-11-22 20:12:14 +0200

    test"

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='learning',
+    version='0.1.0',
+    description='Learning project',
+    author='Mariia Levytska',
+    author_email='mariia.levytska@mail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Specify any dependencies required by your project
+    ],
+)
